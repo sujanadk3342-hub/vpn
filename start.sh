668825c2541ea662b76554752b489eb5ed3f4197
sujanadk3342-hub <sujanadk3342@gmail.com>
2026-04-30 18:41:32 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-wg genkey | tee privatekey | wg pubkey > publickey
+apt update && apt install -y wireguard iproute2 iptables
 
-PRIVATE_KEY=$(cat privatekey)
+PRIVATE_KEY=$(wg genkey)
+PUBLIC_KEY=$(echo "$PRIVATE_KEY" | wg pubkey)
+
+echo "=============================="
+echo "VPN SERVER KEYS"
+echo "=============================="
+echo "PRIVATE KEY:"
+echo "$PRIVATE_KEY"
+echo ""
+echo "PUBLIC KEY:"
+echo "$PUBLIC_KEY"
+echo "=============================="
 
 mkdir -p /etc/wireguard
 
@@ -11,8 +22,6 @@ cat > /etc/wireguard/wg0.conf <<EOF
 PrivateKey = $PRIVATE_KEY
 Address = 10.0.0.1/24
 ListenPort = 51820
-SaveConfig = true
-
 PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 EOF
